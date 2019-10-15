b2ab1e41f38ec23918fec041bcc2321939779c15
Nikita Yustovskiy <yustovskiy@ya.ru>
2019-10-15 14:55:25 +0300

    deploy

---
+++ precache-manifest.1625c34cfb04fab57f30ee4602bc7b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/how-much-per-kilogram/robots.txt"
+  },
+  {
+    "revision": "073a682dcf6ff6ab12ec",
+    "url": "/how-much-per-kilogram/js/chunk-vendors.d0ad73c5.js"
+  },
+  {
+    "revision": "05ce0c885fc4f52565ef",
+    "url": "/how-much-per-kilogram/js/app.f946237e.js"
+  },
+  {
+    "revision": "6e65178c0cb1b22bd979",
+    "url": "/how-much-per-kilogram/js/about.6a28452a.js"
+  },
+  {
+    "revision": "6092521d0eb143036886ab3aed90b18c",
+    "url": "/how-much-per-kilogram/index.html"
+  },
+  {
+    "revision": "05ce0c885fc4f52565ef",
+    "url": "/how-much-per-kilogram/css/app.45d649d5.css"
+  },
+  {
+    "revision": "6e65178c0cb1b22bd979",
+    "url": "/how-much-per-kilogram/css/about.c267412e.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/how-much-per-kilogram/precache-manifest.1625c34cfb04fab57f30ee4602bc7b16.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "how-much-per-kilogram"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
